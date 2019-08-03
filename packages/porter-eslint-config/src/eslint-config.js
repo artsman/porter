0a11b6d2bca1866a567320ca1c8d399ef7fd0469
jharris4 <harris.jb@gmail.com>
2019-08-03 11:02:15 +0100

    allow babel config to be optional for eslint config
    auto inject webpack globals into eslint config

--- packages/porter-eslint-config/src/eslint-config.js
+++ packages/porter-eslint-config/src/eslint-config.js
@@ -1,24 +1,53 @@
 const createBabelConfig = require('@porterjs/babel-config');
 
 module.exports = function createEslintConfig(porterConfig) {
-  const { eslint, babel } = porterConfig;
-  const { files, ...otherConfig } = eslint;
+  const { eslint, babel, webpack } = porterConfig;
+  const { files, useBabel = true, globals, ...otherConfig } = eslint;
 
-  const { parserOptions = {}, ...otherOptions } = otherConfig;
+  let overrideGlobals = globals;
 
-  const { targets, options, presets, plugins } = babel;
-
-  const babelConfig = createBabelConfig({ targets, options, mode: 'test', modules: true, presets, plugins });
-
-  return {
-    ...otherOptions,
-    parserOptions: {
-      ...parserOptions,
-      requireConfigFile: false,
-      babelOptions: {
-        babelrc: false,
-        ...babelConfig
-      }
+  if (webpack) {
+    overrideGlobals = {
+      ...overrideGlobals,
+      process: true
+    };
+    const { defineMap = {} } = webpack;
+    for (let defineKey of Object.keys(defineMap)) {
+      overrideGlobals[defineKey] = true;
     }
-  };
+  }
+
+  if (babel && useBabel) {
+    // TODO - change default parser back to "babel-eslint" once https://github.com/babel/babel-eslint/pull/784 is released
+    const { parser = 'babel-eslint-fork', parserOptions = {}, ...otherOptions } = otherConfig;
+
+    const { targets, options, presets, plugins } = babel;
+
+    const babelConfig = createBabelConfig({ targets, options, mode: 'test', modules: true, presets, plugins });
+
+    return {
+      ...otherOptions,
+      globals: overrideGlobals,
+      parser,
+      parserOptions: {
+        ...parserOptions,
+        requireConfigFile: false,
+        babelOptions: {
+          babelrc: false,
+          ...babelConfig
+        }
+      }
+    };
+  }
+  else {
+    if (overrideGlobals !== globals) {
+      return {
+        ...otherConfig,
+        globals: overrideGlobals
+      };
+    }
+    else {
+      return otherConfig;
+    }
+  }
 };
