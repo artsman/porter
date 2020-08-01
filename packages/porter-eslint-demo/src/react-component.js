f18c569e5959d9b3d18bb2fc347a685a6e85892e
jharris4 <harris.jb@gmail.com>
2020-08-01 11:57:27 +0100

    tweak eslint demo with rest spread usage

--- packages/porter-eslint-demo/src/react-component.js
+++ packages/porter-eslint-demo/src/react-component.js
@@ -7,18 +7,24 @@ export default class MyComponent extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { name: "hello" };
+    this.state = { name: "hello", otherA: "Other A", otherB: "Other B" };
     if (this.state === -0) {
       console.log('deliberately triggered a lint error by comparing with -0');
     }
   }
 
   render() {
-    const { name } = this.state;
+    const { name, ...rest } = this.state;
     
+    const keys = Object.keys(rest);
+    const restString = keys.reduce((s, k) => {
+      s.push(`${k}: ${rest[k]}`);
+    }, []).join(", ");
+
     return (
       <div className="main-container">
-        The name is {name}
+        <span>The name is {name}</span>
+        <span>The rest is {restString}</span>
       </div>);
   }
 }
