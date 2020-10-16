f605ba68df8b38363fc04fdb52dadf5ed0ada80b
jharris4 <harris.jb@gmail.com>
2020-10-16 07:08:36 +0100

    allow passthrough of jest command line args

--- packages/porter-jest/src/jest-exec.js
+++ packages/porter-jest/src/jest-exec.js
@@ -1,10 +1,12 @@
 const jest = require('jest');
 
+const args = (process.argv && process.argv.length > 2) ? process.argv.slice(2) : [];
+
 module.exports = function jestExec({ jestConfig, logger = console }) {
   const newJestConfig = Object.assign({}, jestConfig, {
     transform: {
       "^.+\\.jsx?$": require.resolve('./jest-babel-transform.js')
     }
   });
-  jest.run(['--config', JSON.stringify(newJestConfig)]);
+  jest.run(['--config', JSON.stringify(newJestConfig)].concat(args));
 }
